dbb4fd935dc52330f94cece3b99714f8465a7269
Shashank Sharma <shankreations@gmail.com>
2021-03-14 04:05:12 +0530

    Update virTot.py

--- virTot.py
+++ virTot.py
@@ -3,13 +3,13 @@ import os.path
 from pprint import pprint
 
 API_KEY="YOUR API KEY HERE"
-FILE_PATH = "/home/ubun/Downloads/240387329dee4f03f98a89a2feff9bf30dcba61fcf614cdac24129da54442762"
+FILE_PATH = "FULL PATH TO THE FILE"
 vtotal=Virustotal(API_KEY)
 files = {"file": (os.path.basename(FILE_PATH), open(os.path.abspath(FILE_PATH), "rb"))}
 resp = vtotal.request("file/scan", files=files, method="POST")
 print(resp.response_code)
 pprint(resp.json())
-SCAN_ID = "240387329dee4f03f98a89a2feff9bf30dcba61fcf614cdac24129da54442762-1615673445"
+SCAN_ID = "ENTER YOUR SCAN ID HERE"
 resp = vtotal.request("file/report", {"resource": SCAN_ID})
 print(resp.response_code)
 pprint(resp.json())
